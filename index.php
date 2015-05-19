617df4b7464afd7ed63a728607097c1802989d92
spielhoelle <spiel.hoelle@live.de>
2015-05-19 23:10:46 +0200

    static site

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title of the document</title>
+</head>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+<body>
+The content of the document......
+</body>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+</html> 
